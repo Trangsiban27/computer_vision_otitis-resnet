a4d7d0d83fdb32914a06ffdcb6dd61f808c5b589
TRANG SĨ BÂN <trangsiban@MacBook-Pro-cua-TRANG.local>
2026-06-16 20:37:48 +0700

    Create src/transform file

---
+++ src/transform.py
@@ -0,0 +1,31 @@
+from torchvision import transforms
+
+train_transform = transforms.Compose([
+    transforms.ToPILImage(),
+
+    transforms.Resize((224, 224)),
+
+    transforms.RandomHorizontalFlip(10),
+
+    transforms.RandomRotation(10),
+
+    transforms.ToTensor(),
+
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+val_transform = transforms.Compose([
+    transforms.ToPILImage(),
+
+    transforms.Resize((224, 224,)),
+
+    transforms.ToTensor(),
+
+    transforms.Normalize(
+        mean=[0.485,0.456,0.406],
+        std=[0.229,0.224,0.225]
+    )
+])
